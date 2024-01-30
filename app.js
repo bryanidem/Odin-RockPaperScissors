ec9a270ba4e08a4872236e47d1fbf46d800bb8af
bryanidem <bryan.medina.per@gmail.com>
2024-01-30 08:10:09 -0600

    Add playRound function

---
+++ app.js
@@ -0,0 +1,42 @@
+const getComputerChoice = () => {
+  const options = ["rock", "paper", "scissors"];
+  return options[Math.floor(Math.random() * options.length)];
+};
+
+const playRound = (playerSelection, computerSelection) => {
+  const player = playerSelection.toLowerCase();
+
+  if (player === "rock") {
+    if (computerSelection === "rock") {
+      return "it's a draw ";
+    } else if (computerSelection === "paper") {
+      return "computer wins, rock loses to paper";
+    } else if (computerSelection === "scissors") {
+      return "player wins, rock wins over scissors";
+    }
+  } else if (player === "paper") {
+    if (computerSelection === "rock") {
+      return "player wins, paper wins over paper";
+    } else if (computerSelection === "paper") {
+      return "it's a draw";
+    } else if (computerSelection === "scissors") {
+      return "computer wins, paper loses to scissors";
+    }
+  } else if (player === "scisors") {
+    if (computerSelection === "rock") {
+      return "computer wins, scissors loses to rock";
+    } else if (computerSelection === "paper") {
+      return "player wins, scissors wins over paper";
+    } else if (computerSelection === "scissors") {
+      return "it's a draw";
+    }
+  } else {
+    return "Please write only either [rock] [paper] [scissors]";
+  }
+};
+
+const playerSelection = "Rock";
+const computerSelection = getComputerChoice();
+
+console.log(`player: ${playerSelection} --- computer: ${computerSelection}`);
+console.log(playRound(playerSelection, computerSelection));
